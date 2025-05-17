1d6413068154f7ac41b8f18282b78c284380aca1
nissasyb <akuninn52@gmail.com>
2025-05-17 18:47:20 +0700

    fix

--- src/rot13/data.js
+++ src/rot13/data.js
@@ -1,4 +1,3 @@
-// data.js
 export const alphabetEngUpper = [
     'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
     'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
